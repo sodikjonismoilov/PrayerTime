942752bb657207ca2a99a7cb1c80b134a4e65c8f
Sodikjon Ismoilov <158076064+sodikjonismoilov@users.noreply.github.com>
2026-07-02 21:31:13 -0400

    Initial Commit

---
+++ PrayerTime/PrayerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimeApp.swift
+//  PrayerTime
+//
+//  Created by Sodikjon Ismoilov on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrayerTimeTests/PrayerTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimeTests.swift
+//  PrayerTimeTests
+//
+//  Created by Sodikjon Ismoilov on 7/2/26.
+//
+
+import Testing
+@testable import PrayerTime
+
+struct PrayerTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
